815a2cb4d0d280456757a4c4defe423b77609329
Arya Kashyap <arya.kashyap1908@gmail.com>
2026-08-01 04:51:09 +0530

    remove slash from url

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ from jose import jwt, JWTError
 app = FastAPI()
 
 origins = ["http://localhost:5173",
-           "https://url-shortner-iota-five.vercel.app/",]
+           "https://url-shortner-iota-five.vercel.app",]
 
 app.add_middleware(
     CORSMiddleware,
